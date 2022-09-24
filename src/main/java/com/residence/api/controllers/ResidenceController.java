7a7f5511b8b883e61c0a52fefe0d2f97fa750132
santiago-jv <santiagoolayojv@gmail.com>
2022-09-23 20:03:28 -0500

    fixed relationships x2

--- src/main/java/com/residence/api/controllers/ResidenceController.java
+++ src/main/java/com/residence/api/controllers/ResidenceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.residence.api.models.House;
 import com.residence.api.services.HouseService;
@@ -22,17 +23,9 @@ public class ResidenceController {
 
     @GetMapping("/{residence_id}/house/{id}")
     public ResponseEntity<Object> findHouseById(@PathVariable("residence_id") Long residenceId,@PathVariable("id") Long id) {
+      
         House houseFound = this.houseService.getHouseById(id, residenceId);
-        Map<String, Object> response = new HashMap<>();
-        if(houseFound == null) {
-            response.put("message", "House with id " + id + " not found");
-            response.put("status",  404);
-            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-        }
-        response.put("status", 200);
-        response.put("message", "House found successfully");
-        response.put("data", houseFound);
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        
+        return ResponseEntity.ok().body(houseFound);
     }
 }
--- src/main/java/com/residence/api/services/HouseService.java
+++ src/main/java/com/residence/api/services/HouseService.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.residence.api.models.House;
 import com.residence.api.repositories.HouseRepository;
@@ -20,8 +22,12 @@ public class HouseService {
 
 
     public House getHouseById(Long id, Long residenceId) {
+   
+            
         House queryResponse = this.houseRepository.findHouseByIdAndResidenceId(id, residenceId);
+        if(queryResponse == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "House not found");
         return queryResponse;
+      
     }
         
     
