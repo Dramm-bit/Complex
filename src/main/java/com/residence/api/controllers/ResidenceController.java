7cccd1fbdcc02f428106619f7f2303d1403ea131
Dramm-bit <77645019+Dramm-bit@users.noreply.github.com>
2022-09-25 22:14:42 -0500

    added endpoint update house by residence

--- src/main/java/com/residence/api/controllers/ResidenceController.java
+++ src/main/java/com/residence/api/controllers/ResidenceController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -84,4 +86,12 @@ public class ResidenceController {
         List<House> residencesFound = this.houseService.findHousesinResidenceById(id);
         return ResponseEntity.ok().body(residencesFound);
     }
+    
+    @PutMapping("/{residence_id}/house/{id}")
+    public ResponseEntity<Object> updateHouseBySpecificResidence(@RequestBody() HouseDTO houseData,
+                                                                @PathVariable("residence_id") Long residenceId,
+                                                                @PathVariable("id") Long id){
+    House updatedHouse = this.houseService.updateHouseBySpecificResidence(id, residenceId, houseData);
+    return ResponseEntity.ok().body(updatedHouse);
+}
 }
--- src/main/java/com/residence/api/services/ResidenceService.java
+++ src/main/java/com/residence/api/services/ResidenceService.java
@@ -1,13 +1,12 @@
 package com.residence.api.services;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
+
 
 import com.residence.api.dataTranferObjects.ResidenceDTO;
 import com.residence.api.models.PaymentConfig;
